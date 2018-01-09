b4fc48902d1a7d8b53469e44c3011c6773369738
Mehni <Mehni@users.noreply.github.com>
2018-01-09 15:09:24 +0100

    B18 ready

--- Cutepenguin/Penguin_JobGiver_Cleaning.cs
+++ Cutepenguin/Penguin_JobGiver_Cleaning.cs
@@ -2,22 +2,26 @@
 using Verse;
 using Verse.AI;
 using RimWorld;
+using System.Collections.Generic;
 
 namespace Penguin
 {
     public class Penguin_JobGiver_Cleaning : ThinkNode_JobGiver
     {
+
         protected override Job TryGiveJob(Pawn pawn)
         {
         Predicate<Thing> filth = t => t.def.category == ThingCategory.Filth;
 
-        Thing thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Filth), PathEndMode.ClosestTouch, 
-            TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false), 300f, filth, null, 0, -1, false, RegionType.Set_Passable, false);
+            Thing thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Filth), PathEndMode.ClosestTouch,
+                TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false), 300f, filth, null, 0, -1, false, RegionType.Set_Passable, false);
 
-        if (thing != null)
-        {
-            return new Job(JobDefOf.Clean, thing);
-        }
+            if (thing != null && pawn.CanReserve(thing))
+            {
+                Job job = new Job(JobDefOf.Clean);
+                job.AddQueuedTarget(TargetIndex.A, thing);
+                return job;
+            }
         return null;
         }
     }
